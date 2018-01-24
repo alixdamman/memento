9954394d85927db5fd97c81d919aeb1f2691eb0e
Alix Damman <ald@plan.be>
2018-01-24 15:53:11 +0100

    updated exercices associated with functions (round 2)

--- python/functions/model_as_function.py
+++ python/functions/model_as_function.py
@@ -1,32 +1,30 @@
 from larray import *
 
-def main():
-    #define constants
-    FIRST_YEAR_OBS = 1991
-    FIRST_YEAR_PROJ = 2016
-    LAST_YEAR_PROJ = 2060
-    YEAR = Axis('year={}..{}'.format(FIRST_YEAR_OBS, LAST_YEAR_PROJ))
+# define constants
+FIRST_YEAR_OBS = 1991
+FIRST_YEAR_PROJ = 2016
+LAST_YEAR_PROJ = 2060
+YEAR = Axis('year={}..{}'.format(FIRST_YEAR_OBS, LAST_YEAR_PROJ))
 
+
+def main():
     # load input
     pop = read_csv("input/pop_region.csv")
 
-    # start projection with coef = 1.01
-    coef = 1.01
+    # start projection
     proj_pop1 = pop.reindex("year", YEAR, fill_value=0)
     for year in YEAR[FIRST_YEAR_PROJ:]:
-        proj_pop1[year] = proj_pop1[year - 1] * coef
+        proj_pop1[year] = proj_pop1[year - 1] * 1.01
 
-    # start projection with coef = 1.02
-    coef = 1.02
+    # start projection with a new coefficient value
     proj_pop2 = pop.reindex("year", YEAR, fill_value=0)
     for year in YEAR[FIRST_YEAR_PROJ:]:
-        proj_pop2[year] = proj_pop2[year - 1] * coef
+        proj_pop2[year] = proj_pop2[year - 1] * 1.02
 
-    # start projection with coef = 1.03
-    coef = 1.03
+    # start projection with a new coefficient value
     proj_pop3 = pop.reindex("year", YEAR, fill_value=0)
     for year in YEAR[FIRST_YEAR_PROJ:]:
-        proj_pop3[year] = proj_pop3[year - 1] * coef
+        proj_pop3[year] = proj_pop3[year - 1] * 1.03
 
     # compare the projections
     compare(proj_pop1, proj_pop2, proj_pop3)
